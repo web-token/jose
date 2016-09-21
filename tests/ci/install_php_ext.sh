8e5f728dd04b998eb57b397fe4cc30aec5cfb172
Florent Morselli <florent+git@morselli.fr>
2016-09-21 22:21:03 +0200

    Bug fixed

--- tests/ci/install_php_ext.sh
+++ tests/ci/install_php_ext.sh
@@ -7,6 +7,7 @@ phpize
 make
 sudo make install
 cd ..
+rm -rf php-curve25519-ext
 echo "extension = curve25519.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 git clone git://github.com/encedo/php-ed25519-ext.git
 cd php-ed25519-ext
@@ -15,4 +16,5 @@ phpize
 make
 sudo make install
 cd ..
+rm -rf php-ed25519-ext
 echo "extension = ed25519.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
