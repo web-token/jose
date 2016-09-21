915cef9c2cf3c85618ac8a69e6c8d7130a5a7e6b
Florent Morselli <florent+git@morselli.fr>
2016-09-21 22:18:21 +0200

    PHP Extension installed

---
+++ tests/ci/install_php_ext.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git clone git://github.com/lt/php-curve25519-ext.git
+cd php-curve25519-ext
+phpize
+./configure
+make
+sudo make install
+cd ..
+echo "extension = curve25519.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+git clone git://github.com/encedo/php-ed25519-ext.git
+cd php-ed25519-ext
+phpize
+./configure
+make
+sudo make install
+cd ..
+echo "extension = ed25519.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
