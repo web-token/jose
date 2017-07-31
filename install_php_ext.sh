d2cff56807231abfa66cf63899a5e30481fdf6eb
Spomky <florent@morselli.fr>
2017-07-31 13:32:56 +0200

    Travis script for PHP 7.1

---
+++ install_php_ext.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:chris-lea/libsodium
+sudo apt-get update
+sudo apt-get install libsodium-dev build-essential
+git clone -b stable https://github.com/jedisct1/libsodium.git
+cd libsodium
+./configure
+make check
+sudo make install
+pecl install libsodium
+echo "extension = sodium.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+cd ../
