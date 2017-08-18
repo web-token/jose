6a0f3cafe42ceb9cc99aef74b7536ac2a5178fc9
Spomky <florent@morselli.fr>
2017-08-19 01:37:49 +0200

    Update & Rollback support

---
+++ src/Component/Console/Command/RollbackCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Humbug\SelfUpdate\Updater;
+
+final class RollbackCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('rollback')
+            ->setDescription('Rollback current version.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $updater = new Updater();
+        try {
+            $result = $updater->rollback();
+            if (!$result) {
+                echo "Failure!\n";
+                exit(1);
+            }
+            echo "Success!\n";
+        } catch (\Exception $e) {
+            echo "Well, something happened! Either an oopsie or something involving hackers.\n";
+            exit(1);
+        }
+    }
+}
---
+++ src/Component/Console/Command/UpdateCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Humbug\SelfUpdate\Updater;
+
+final class UpdateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('selfupdate')
+            ->setDescription('Update the application if needed.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $updater = new Updater();
+        $updater->getStrategy()->setPharUrl('https://spomky-labs.github.io/jose/jose.phar');
+        $updater->getStrategy()->setVersionUrl('https://spomky-labs.github.io/jose/jose.phar.version');
+        try {
+            $result = $updater->update();
+            if ($result) {
+                $new = $updater->getNewVersion();
+                $old = $updater->getOldVersion();
+                printf(
+                    'Updated from SHA-1 %s to SHA-1 %s', $old, $new
+                );
+            } else {
+                echo "No update needed!\n";
+            }
+        } catch (\Exception $e) {
+            echo "Well, something happened! Either an oopsie or something involving hackers.\n";
+            exit(1);
+        }
+    }
+}
