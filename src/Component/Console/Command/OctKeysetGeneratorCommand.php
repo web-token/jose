eb2dfccea7182dc44bce51aa0d9f68cc43b2ba33
Spomky <florent@morselli.fr>
2017-08-22 22:38:03 +0200

    Auto deploy removed and new commands added

---
+++ src/Component/Console/Command/OctKeysetGeneratorCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Console\Command;
+
+use Jose\Component\Core\JWKSet;
+use Jose\Component\KeyManagement\JWKFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class OctKeysetGeneratorCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('keyset:generate:oct')
+            ->setDescription('Generate a key set with octet keys (JWK format)')
+            ->setDefinition(
+                new InputDefinition([
+                    new InputOption('quantity', null, InputOption::VALUE_REQUIRED, 'Quantity of keys in the key set.'),
+                    new InputOption('size', 's', InputOption::VALUE_OPTIONAL, 'Key size.', 2048),
+                    new InputOption('use', 'u', InputOption::VALUE_OPTIONAL, 'Usage of the key. Must be either "sig" or "enc".'),
+                    new InputOption('alg', 'a', InputOption::VALUE_OPTIONAL, 'Algorithm for the key.'),
+                    new InputOption('out', 'o', InputOption::VALUE_OPTIONAL, 'File where to save the key. Must be a valid and writable file name.'),
+                ])
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $quantity = (int) $input->getOption('quantity');
+        $size = (int) $input->getOption('size');
+        $args = [];
+        foreach (['use', 'alg'] as $key) {
+            $value = $input->getOption($key);
+            if (null !== $value) {
+                $args[$key] = $value;
+            }
+        }
+
+        $keyset = JWKSet::createFromKeys([]);
+        for ($i = 0 ; $i < $quantity ; ++$i) {
+            $keyset = $keyset->withKey(JWKFactory::createOctKey($size, $args));
+        }
+        $json = json_encode($keyset);
+
+        $file = $input->getOption('out');
+        if (null !== $file) {
+            file_put_contents($file, $json, LOCK_EX);
+        } else {
+            $output->write($json);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return class_exists('\Jose\Component\KeyManagement\JWKFactory');
+    }
+}
---
+++ src/Component/Console/Command/P12CertificateLoaderCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Jose\Component\Console\Command;
+
+use Jose\Component\KeyManagement\JWKFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class P12CertificateLoaderCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('key:load:p12')
+            ->setDescription('Load a key from a P12 certificate file.')
+            ->setDefinition(
+                new InputDefinition([
+                    new InputOption('file', 'f', InputOption::VALUE_REQUIRED, 'Filename of the P12 certificate.'),
+                    new InputOption('secret', 's', InputOption::VALUE_OPTIONAL, 'Secret if the key is encrypted.'),
+                    new InputOption('use', 'u', InputOption::VALUE_OPTIONAL, 'Usage of the key. Must be either "sig" or "enc".'),
+                    new InputOption('alg', 'a', InputOption::VALUE_OPTIONAL, 'Algorithm for the key.'),
+                    new InputOption('out', 'o', InputOption::VALUE_OPTIONAL, 'File where to save the key. Must be a valid and writable file name.'),
+                ])
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filename = $input->getOption('file');
+        $password = $input->getOption('secret');
+        $args = [];
+        foreach (['use', 'alg'] as $key) {
+            $value = $input->getOption($key);
+            if (null !== $value) {
+                $args[$key] = $value;
+            }
+        }
+
+        $jwk = JWKFactory::createFromPKCS12CertificateFile($filename, $password, $args);
+        $json = json_encode($jwk);
+
+        $file = $input->getOption('out');
+        if (null !== $file) {
+            file_put_contents($file, $json, LOCK_EX);
+        } else {
+            $output->write($json);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return class_exists('\Jose\Component\KeyManagement\JWKFactory');
+    }
+}
