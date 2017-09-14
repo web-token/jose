cfb8731963a905c34bca2784951b1274f9ddb4f6
Spomky <Spomky@users.noreply.github.com>
2017-09-14 13:06:16 +0200

    Apply fixes from StyleCI (#244)
    [ci skip] [skip ci]

--- src/Component/Core/Util/Ecc/Math/GmpMath.php
+++ src/Component/Core/Util/Ecc/Math/GmpMath.php
@@ -215,7 +215,7 @@ final class GmpMath
         $result = gmp_init(0, 10);
         $sLen = mb_strlen($s, '8bit');
 
-        for ($c = 0; $c < $sLen; ++$c ) {
+        for ($c = 0; $c < $sLen; ++$c) {
             $result = gmp_add(gmp_mul(256, $result), gmp_init(ord($s[$c]), 10));
         }
 
